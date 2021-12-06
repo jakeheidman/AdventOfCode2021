50701fd681fc36d5314750baa9f17292cc6eed0d
Jake Heidman <jake.heidman2@gmail.com>
2021-12-06 17:44:40 -0500

    Day 6 Part 1 & 2

--- AdventOfCode2021/Day6/Day6.swift
+++ AdventOfCode2021/Day6/Day6.swift
@@ -6,37 +6,47 @@
 //
 import Foundation
 
+//func parse_day6(input: String) -> [Int] {
+//    let timers: [Int] = input.components(separatedBy: ",").map({Int($0) ?? 0})
+//    return timers
+//}
+/*
+ @param input: "3,4,1,2,1,2,2,4,0"
+ @return fish: number of fish of each age, where age is the index [1, 2, 3, 1, 2]
+ (1 age 0 fish, 2 age 1 fish, etc
+ */
 func parse_day6(input: String) -> [Int] {
     let timers: [Int] = input.components(separatedBy: ",").map({Int($0) ?? 0})
-    return timers
+    var fish = [Int](repeating: 0, count: 9)
+    for f in timers {
+        fish[f] += 1
+    }
+    return fish
 }
 
 func day6_part1() -> Int {
     let lanternfish: [Int] = parse_day6(input: input6)
-    return simulate_lanternfish(lanternfish: lanternfish, generations: 256)
+    return simulate_generations(lanternfish: lanternfish, generations: 80)
 }
 
-func simulate_lanternfish(lanternfish: [Int], generations: Int) -> Int {
-    var newLanternFish: [Int] = lanternfish
-    for gen in 0...generations-1 {
-        print(gen)
-        newLanternFish = generation_simulate(lanternfish: newLanternFish)
-    }
-    return newLanternFish.count
+func day6_part2() -> Int {
+    let lanternfish: [Int] = parse_day6(input: input6)
+    return simulate_generations(lanternfish: lanternfish, generations: 256)
 }
 
-func generation_simulate(lanternfish: [Int]) -> [Int] {
-    var updatedLanternFish = lanternfish
-    var newFish: [Int] = []
-    for fish in 0...lanternfish.count-1 {
-        if updatedLanternFish[fish] == 0 {
-            newFish.append(8)
-            updatedLanternFish[fish] = 6
+func simulate_generations(lanternfish: [Int], generations: Int) -> Int {
+    var currentGeneration = lanternfish
+    for _ in 1...generations {
+        var updatedGeneration = [Int](repeating: 0, count: 9)
+        //spawn new fish from gen 0
+        updatedGeneration[8] += currentGeneration[0]
+        updatedGeneration[6] += currentGeneration[0]
+        
+        //loop through ages 1-8
+        for idx in 0...currentGeneration.count - 2 {
+            updatedGeneration[idx] += currentGeneration[idx+1]
         }
-        else {
-            updatedLanternFish[fish] -= 1
-        }
+        currentGeneration = updatedGeneration
     }
-    updatedLanternFish += newFish
-    return updatedLanternFish
+    return currentGeneration.reduce(0, +)
 }
