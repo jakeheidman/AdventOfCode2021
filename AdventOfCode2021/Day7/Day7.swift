6c8a10b1fc54566393b0f2b7e4022ea4a045ec48
Jake Heidman <jake.heidman2@gmail.com>
2021-12-07 17:15:50 -0500

    day 7 clean up

--- AdventOfCode2021/Day7/Day7.swift
+++ AdventOfCode2021/Day7/Day7.swift
@@ -6,45 +6,38 @@
 //
 
 import Foundation
-
+/*
+ Sorts & converts input string to [Int]
+ "1,7,4,9,3" -> [1, 3, 4, 7, 9]
+ */
 func parse_day7(input: String) -> [Int] {
     let crabs: [Int] = input.components(separatedBy: ",").map({Int($0) ?? 0}).sorted()
     return crabs
 }
 
-func day7_part1() -> Int {
-    let crabs = parse_day7(input: input7)
-    var lowest_fuel = Int.max
-    for c in crabs {
-        let alignToC = calculate_static_fuel(crabs: crabs, individual: c)
-        if alignToC < lowest_fuel {
-            lowest_fuel = alignToC
-        }
-    }
-    return lowest_fuel
-}
 /*
  Efficient implementation of Part I
  The median of a set of numbers will always minimize
  the sum of absolute deviations
  */
-func day7_part1_efficient() -> Int {
+func day7_part1() -> Int {
     let crabs = parse_day7(input: input7)
     let alignTo = Int(calculateMedian(array: crabs))
     return calculate_static_fuel(crabs: crabs, individual: alignTo)
 }
+
 /*
  Efficient implementation of Part II
  The avg of a set is a close enough approximation of
  the sequence n*(n+1)/2, which is the dynamic fuel cost. You can
- approximate that, and then check the averages neighbors to account for
- rounding to an integer to find the lowest cost.
+ approximate that, and then check only the avg and avg + 1 to account for rounding
+ (ie if avg was 4.6 we would want to check both 4 and 5, since calculateAverage would return 4).
  */
-func day7_part2_efficient() -> Int{
+func day7_part2() -> Int{
     let crabs = parse_day7(input: input7)
     let avg = calculateAverage(array: crabs)
     var lowestFuel = Int.max
-    for alignPosition in avg-1...avg+1 {
+    for alignPosition in avg...avg+1 {
         let fuel = calculate_dynamic_fuel(crabs: crabs, individual: alignPosition)
         if fuel < lowestFuel {
             lowestFuel = fuel
@@ -53,12 +46,14 @@ func day7_part2_efficient() -> Int{
     return lowestFuel
 }
 
+/*
+ returns median of sorted array
+ */
 func calculateMedian(array: [Int]) -> Float {
-    let sorted = array.sorted()
-    if sorted.count % 2 == 0 {
-        return Float((sorted[(sorted.count / 2)] + sorted[(sorted.count / 2) - 1])) / 2
+    if array.count % 2 == 0 {
+        return Float((array[(array.count / 2)] + array[(array.count / 2) - 1])) / 2
     } else {
-        return Float(sorted[(sorted.count - 1) / 2])
+        return Float(array[(array.count - 1) / 2])
     }
 }
 
@@ -66,17 +61,6 @@ func calculateAverage(array: [Int]) -> Int {
     return array.reduce(0, +) / array.count
 }
 
-func day7_part2() -> Int {
-    let crabs = parse_day7(input: input7)
-    var lowest_fuel = Int.max
-    for c in crabs[0]...crabs[crabs.count-1] {
-        let alignToC = calculate_dynamic_fuel(crabs: crabs, individual: c)
-        if alignToC < lowest_fuel {
-            lowest_fuel = alignToC
-        }
-    }
-    return lowest_fuel
-}
 
 func calculate_static_fuel(crabs: [Int], individual: Int) -> Int {
     var fuel = 0
@@ -99,7 +83,7 @@ func dynamicAlignCost(original: Int, alignTo: Int) -> Int {
     var totalCost = 0
     var stepCost = 1
     var smaller = original > alignTo ? alignTo : original
-    var bigger = original > alignTo ? original : alignTo
+    let bigger = original > alignTo ? original : alignTo
     while smaller != bigger {
         smaller += 1
         totalCost += stepCost
