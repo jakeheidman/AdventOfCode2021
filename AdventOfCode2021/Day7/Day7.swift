acd4b43ee3ac1d2278e2a84eef5118557f6ef98b
Jake Heidman <jake.heidman2@gmail.com>
2021-12-07 15:06:18 -0500

    efficient day 7 solutions

--- AdventOfCode2021/Day7/Day7.swift
+++ AdventOfCode2021/Day7/Day7.swift
@@ -23,6 +23,48 @@ func day7_part1() -> Int {
     }
     return lowest_fuel
 }
+/*
+ Efficient implementation of Part I
+ The median of a set of numbers will always minimize
+ the sum of absolute deviations
+ */
+func day7_part1_efficient() -> Int {
+    let crabs = parse_day7(input: input7)
+    let alignTo = Int(calculateMedian(array: crabs))
+    return calculate_static_fuel(crabs: crabs, individual: alignTo)
+}
+/*
+ Efficient implementation of Part II
+ The avg of a set is a close enough approximation of
+ the sequence n*(n+1)/2, which is the dynamic fuel cost. You can
+ approximate that, and then check the averages neighbors to account for
+ rounding to an integer to find the lowest cost.
+ */
+func day7_part2_efficient() -> Int{
+    let crabs = parse_day7(input: input7)
+    let avg = calculateAverage(array: crabs)
+    var lowestFuel = Int.max
+    for alignPosition in avg-1...avg+1 {
+        let fuel = calculate_dynamic_fuel(crabs: crabs, individual: alignPosition)
+        if fuel < lowestFuel {
+            lowestFuel = fuel
+        }
+    }
+    return lowestFuel
+}
+
+func calculateMedian(array: [Int]) -> Float {
+    let sorted = array.sorted()
+    if sorted.count % 2 == 0 {
+        return Float((sorted[(sorted.count / 2)] + sorted[(sorted.count / 2) - 1])) / 2
+    } else {
+        return Float(sorted[(sorted.count - 1) / 2])
+    }
+}
+
+func calculateAverage(array: [Int]) -> Int {
+    return array.reduce(0, +) / array.count
+}
 
 func day7_part2() -> Int {
     let crabs = parse_day7(input: input7)
